341ae764decac761f3ffe520da891f9135cfec24
xuxialei <2442313112@qq.com>
2018-11-14 20:56:57 +0800

    first push

---
+++ src/main/java/cn/xxl/LuceneTest/Test.java
@@ -0,0 +1,24 @@
+package cn.xxl.LuceneTest;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Date;
+
+
+
+public class Test {
+
+	public void TextIndex() throws Exception{
+
+	}
+	public static void main(String[] args) throws IOException {
+       
+
+        
+    }
+
+}
